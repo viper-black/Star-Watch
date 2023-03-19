07f8655d677ebb946eea52e2dc81efdd3efa536b
viper-black <94998319+viper-black@users.noreply.github.com>
2023-03-19 23:34:52 +0100

    Added point system

--- Assets/Scripts/PhotoScript.cs
+++ Assets/Scripts/PhotoScript.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PhotoScript : MonoBehaviour
 {
     Camera cam;
     Plane[] cameraFrustum;
     Collider collider;
+
+    [SerializeField] PointScript ps;
+    [SerializeField] int PointWorth;
+
+    [SerializeField] float delayTime = 10f;
+    bool delayOver = true;
+
+    public InputActionProperty takePhoto;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,15 +26,35 @@ public class PhotoScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        TakePicture();
+    }
+    void TakePicture()
+    {
+        float trigger = takePhoto.action.ReadValue<float>();
+
         var bounds = collider.bounds;
         cameraFrustum = GeometryUtility.CalculateFrustumPlanes(cam);
-        if(GeometryUtility.TestPlanesAABB(cameraFrustum, bounds))
+        if (trigger > 0.5f && delayOver == true)
         {
-            GetComponent<MeshRenderer>().material.color = Color.green;
+            delayOver = false;
+            Invoke("Delay", delayTime);
+            if (GeometryUtility.TestPlanesAABB(cameraFrustum, bounds))
+            {
+                int pointsToAdd = PointWorth - Mathf.RoundToInt(cam.fieldOfView);
+                GetComponent<MeshRenderer>().material.color = Color.green;
+                if (pointsToAdd > 0)
+                {
+                    ps.addPoints(pointsToAdd);
+                }
+            }
+            else
+            {
+                GetComponent<MeshRenderer>().material.color = Color.red;
+            }
         }
-        else
-        {
-            GetComponent<MeshRenderer>().material.color = Color.red;
-        }
+    }
+    void Delay()
+    {
+        delayOver = true;
     }
 }
